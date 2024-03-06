a593b75dbc4aee16b32840d3a4cb402bfcd203e1
svCcare <maciej.pawlak94@gmail.com>
2024-03-06 22:34:03 +0100

    Added CollectionExtensions and WithIndex method

---
+++ AdventOfSharedTools.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfSharedTools.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void WithIndex_AssignsIndexCorrectly()
+        {
+            var collection = new string[] { "a", "b", "c" };
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var (item, index) in collection.WithIndex())
+            {
+                dictionary.Add(item, index);
+            }
+
+            dictionary.Should().HaveCount(collection.Length);
+            dictionary["a"].Should().Be(0);
+            dictionary["b"].Should().Be(1);
+            dictionary["c"].Should().Be(2);
+        }
+
+        [Fact]
+        public void WithIndex_ShouldThrowException_IfCollectionIsNull()
+        {
+            List<string> collection = null;
+
+            Action act = () => collection.WithIndex();
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void WithIndex_ShouldNotThrowException_IfCollectionIsEmpty()
+        {
+            var collection = Enumerable.Empty<string>();
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var (item, index) in collection.WithIndex())
+            {
+                dictionary.Add(item, index);
+            }
+
+            dictionary.Should().BeEmpty();
+        }
+    }
+}
---
+++ AdventOfSharedTools/CollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfSharedTools
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Foreach iteration but gives access to current item index. Uses tuple object (T item, int index)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            return source.Select((item, index) => (item, index));
+        }
+    }
+}
