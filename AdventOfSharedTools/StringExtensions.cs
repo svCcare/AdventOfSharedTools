96325ec41ab16b63b67c735462a1e85c60dd979e
svCcare <maciej.pawlak94@gmail.com>
2024-03-07 18:56:06 +0100

    Added string extension class with ReplaceWhitespace method

---
+++ AdventOfSharedTools/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfSharedTools
+{
+    public static class StringExtensions
+    {
+        private static readonly Regex sWhitespace = new Regex(@"\s+");
+
+        /// <summary>
+        /// Use to replace each occurance of a whitespace with given string. If replacement is not given, whitespaces are removed.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="replacement"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="RegexMatchTimeoutException"></exception>
+        /// <returns></returns>
+        public static string ReplaceWhitespace(this string input, string replacement = "")
+        {
+            return sWhitespace.Replace(input, replacement);
+        }
+    }
+}
