2f977cc03755e331a5157787aacf2111b001d031
svCcare <maciej.pawlak94@gmail.com>
2024-03-12 00:09:47 +0100

    Documenting a method

--- AdventOfSharedTools/Mappers/DirectionMapper.cs
+++ AdventOfSharedTools/Mappers/DirectionMapper.cs
@@ -2,8 +2,26 @@
 
 namespace AdventOfSharedTools.Mappers
 {
+    /// <summary>
+    /// Allows mapping to Direction object.
+    /// </summary>
     public static class DirectionMapper
     {
+        /// <summary>
+        /// <para>Maps character onto Direction object.</para>
+        /// Currently supported characters:
+        /// <list type="bullet">
+        /// <c>
+        ///    <item> <description>'U' or 'N' => Direction.Up</description> </item>
+        ///    <item> <description>'D' or 'S' => Direction.Down</description> </item>
+        ///    <item> <description>'R' or 'E' => Direction.Right</description> </item>
+        ///    <item> <description>'L' or 'W' => Direction.Left</description> </item>
+        ///    </c>
+        ///</list>
+        /// </summary>
+        /// <param name="input">Input to map. Case does not matter.</param>
+        /// <returns>Direction object.</returns>
+        /// <exception cref="ArgumentException"></exception>
         public static Direction FromChar(char input) =>
             input.ToString().ToUpper()[0] switch
             {
