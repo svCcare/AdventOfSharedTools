b783bfa72dd5bab1c99d6980d51671c294036fc7
svCcare <maciej.pawlak94@gmail.com>
2024-03-18 23:05:26 +0100

    Added two enums that will help with card and poker related challenges

---
+++ AdventOfSharedTools/Models/CardType.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfSharedTools.Models
+{
+    /// <summary>
+    /// Stores all types of cards starting from the lowest value (2) finishing on Ace (14).
+    /// </summary>
+    public enum CardType
+    {
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+}
---
+++ AdventOfSharedTools/Models/PokerHandResult.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfSharedTools.Models
+{
+    /// <summary>
+    /// Contains all possible Poker Hands starting from the lowest up to highest possible being Royal Flush.
+    /// </summary>
+    public enum PokerHandResult
+    {
+        None,
+        Pair,
+        TwoPairs,
+        ThreeKind,
+        Straight,
+        Flush,
+        FullHouse,
+        FourKind,
+        FiveKind,
+        StraightFlush,
+        RoyalFlush,
+    }
+}
