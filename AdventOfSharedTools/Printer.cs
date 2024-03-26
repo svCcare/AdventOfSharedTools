d77750b739339cc81ef755ca45125b179df9b291
svCcare <maciej.pawlak94@gmail.com>
2024-03-26 18:28:59 +0100

    Added static method to print well formatted results

---
+++ AdventOfSharedTools/Printer.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfSharedTools
+{
+    public static class Printer
+    {
+        public static void PrintResult(int result)
+        {
+            Console.WriteLine($"Result: {result}");
+        }
+
+        public static void PrintResults(int resultPartOne = 0, int resultPartTwo = 0)
+        {
+            Console.WriteLine($"Part 1: {resultPartOne}");
+            Console.WriteLine($"Part 2: {resultPartTwo}");
+        }
+
+        public static void PrintResults(int resultPartOne = 0, int resultPartTwo = 0, int timeElapsedPartOne = 0, int timeElapsedPartTwo = 0)
+        {
+            Console.WriteLine($"Part 1: {resultPartOne} | Solved in: {timeElapsedPartOne}ms");
+            Console.WriteLine($"Part 2: {resultPartTwo} | Solved in: {timeElapsedPartTwo}ms");
+        }
+    }
+}
