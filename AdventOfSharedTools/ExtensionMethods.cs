73ed157789287da47154a6be246a2d72ebfa5cd7
svCcare <maciej.pawlak94@gmail.com>
2024-03-29 15:52:01 +0100

    Documenting ExtensionMethods

--- AdventOfSharedTools/ExtensionMethods.cs
+++ AdventOfSharedTools/ExtensionMethods.cs
@@ -4,6 +4,21 @@ namespace AdventOfSharedTools
 {
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// <para>Use to obtain more visual representation of Card Shape.</para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <c>
+        ///    <item> <description>CardShape.Heart => ♥</description> </item>
+        ///    <item> <description>CardShape.Diamond => ♦</description> </item>
+        ///    <item> <description>CardShape.Club => ♣</description> </item>
+        ///    <item> <description>CardShape.Spade => ♠</description> </item>
+        ///</c>
+        ///</list>
+        /// </summary>
+        /// <param name="shape"></param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException"></exception>
         public static char CardShapeAsIcon(this CardShape shape)
         {
             return shape switch
@@ -16,6 +31,22 @@ namespace AdventOfSharedTools
             };
         }
 
+        /// <summary>
+        /// <para>Joins a number to another. Will throw <c>ArgumentException</c> if joined number extends Integer range or joined number is negative.</para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <c>
+        ///    <item> <description>0 + 1 => 1</description> </item>
+        ///    <item> <description>1 + 1 => 11</description> </item>
+        ///    <item> <description>0 + 0 => 0</description> </item>
+        ///    <item> <description>1 + 0 => 10</description> </item>
+        ///</c>
+        ///</list>
+        ///</summary>
+        /// <param name="number"></param>
+        /// <param name="anotherNumber"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static int JoinNumber(this int number, int anotherNumber)
         {
             var mergedAsString = number.ToString() + anotherNumber.ToString();
