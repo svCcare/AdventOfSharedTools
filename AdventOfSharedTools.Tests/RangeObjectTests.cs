c3b2d0ae69544a64353b1ba31511fe162a81056b
svCcare <maciej.pawlak94@gmail.com>
2024-03-31 22:14:10 +0200

    RangeObject model added with simple test

---
+++ AdventOfSharedTools.Tests/RangeObjectTests.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfSharedTools.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfSharedTools.Tests;
+
+public class RangeObjectTests
+{
+    [Fact]
+    public void Contains_Works()
+    {
+        var range = new RangeObject<int>(5, 8);
+
+        var result = range.Contains(6);
+
+        result.Should().BeTrue();
+    }
+}
---
+++ AdventOfSharedTools/Models/RangeObject.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfSharedTools.Models;
+
+public record RangeObject<T>(T Start, T End) where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+{
+    public bool Contains(T value)
+    {
+        return Comparer<T>.Default.Compare(value, Start) > 0 && Comparer<T>.Default.Compare(value, End) < 0;
+    }
+}
