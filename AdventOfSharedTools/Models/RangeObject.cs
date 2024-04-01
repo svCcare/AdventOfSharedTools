0b7d5a769c70629ba52fad4b74659315bc0d7ceb
svCcare <maciej.pawlak94@gmail.com>
2024-04-01 19:44:09 +0200

    Documented the code

--- AdventOfSharedTools/Models/RangeObject.cs
+++ AdventOfSharedTools/Models/RangeObject.cs
@@ -1,7 +1,19 @@
 ﻿namespace AdventOfSharedTools.Models;
 
+/// <summary>
+/// Model representing open range with start and end value.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="Start"></param>
+/// <param name="End"></param>
 public record RangeObject<T>(T Start, T End) where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
 {
+    /// <summary>
+    /// Checks if given value lays between start and end values of this object.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
     public bool Contains(T value)
     {
         return Comparer<T>.Default.Compare(value, Start) > 0 && Comparer<T>.Default.Compare(value, End) < 0;
