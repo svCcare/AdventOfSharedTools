fd07198d9bdfda65a6560758cd49a5aa6ab7ac47
svCcare <maciej.pawlak94@gmail.com>
2024-04-03 21:28:37 +0200

    ShoelaceTheoremTests

---
+++ AdventOfSharedTools.Tests/ShoelaceTheoremTestData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace AdventOfSharedTools.Tests
+{
+    internal class ShoelaceTheoremTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new int[][]
+                {
+                    [2,3],
+                    [5,5],
+                    [8,3],
+                    [7,1],
+                },
+                12
+            };
+
+            //yield return new object[]
+            //{
+            //    new Location[]
+            //    {
+            //        new(0,0),
+            //    }
+            //};
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- AdventOfSharedTools.Tests/ShoelaceTheoremTests.cs
+++ AdventOfSharedTools.Tests/ShoelaceTheoremTests.cs
@@ -1,13 +1,44 @@
-﻿using Xunit;
+﻿using AdventOfSharedTools.Algorythms;
+using AdventOfSharedTools.Models;
+using FluentAssertions;
+using System.Drawing;
+using Xunit;
 
 namespace AdventOfSharedTools.Tests
 {
     public class ShoelaceTheoremTests
     {
-        [Fact]
-        public void Method()
+        [Theory]
+        [ClassData(typeof(ShoelaceTheoremTestData))]
+        public void Calculate_ForLocation_YieldsCorrectResult(IEnumerable<int[]> points, long expectedArea)
         {
+            var asLocation = points.Select(x => new Location(x[0], x[1])).ToList();
 
+            var result = ShoelaceTheorem.Calculate(asLocation);
+
+            result.Should().Be(expectedArea);
+        }
+
+        [Theory]
+        [ClassData(typeof(ShoelaceTheoremTestData))]
+        public void Calculate_ForPoint_YieldsCorrectResult(IEnumerable<int[]> points, long expectedArea)
+        {
+            var asPoint = points.Select(x => new Point(x[0], x[1])).ToList();
+
+            var result = ShoelaceTheorem.Calculate(asPoint);
+
+            result.Should().Be(expectedArea);
+        }
+
+        [Theory]
+        [ClassData(typeof(ShoelaceTheoremTestData))]
+        public void Calculate_ForTuple_YieldsCorrectResult(IEnumerable<int[]> points, long expectedArea)
+        {
+            var asTuple = points.Select(x => (x[0], x[1])).ToList();
+
+            var result = ShoelaceTheorem.Calculate(asTuple);
+
+            result.Should().Be(expectedArea);
         }
     }
 }
